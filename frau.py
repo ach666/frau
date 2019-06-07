e9201b60a188055c3c827b53ad2ea2eee5425d23
ach666 <46721480+ach666@users.noreply.github.com>
2019-06-07 05:14:00 -0700

    Update frau.py

--- frau.py
+++ frau.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import asyncio
 
 client = discord.Client()
 
@@ -7,6 +8,15 @@ client = discord.Client()
 async def on_ready():
   await client.change_presence(game=discord.Game(name="porno make me horno"))
 
+@client.event
+async def on_message(message):
+
+    if message.author != client.user:
+      if ("training" in message.content.lower()) or ("frau" in message.content.lower()) or ("sex" in message.content.lower()):
+        await client.send_message(message.channel,"OHHHHHHHHHHHHHHH YEAHHHHHHHHH!!!!!!!!!!!!!!!!!!!!!!")
+       
+      if ("poop" in message.content.lower()):
+        await client.send_message(message.channel, "I'M SHIDDING. SHIDDING AND FARDING.")
 
 token = os.environ.get("DISCORD_BOT_SECRET")
 client.run(token)
