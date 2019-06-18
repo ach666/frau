60990dadaea0f6d56431230126f2c1cbe1292b73
ach666 <46721480+ach666@users.noreply.github.com>
2019-06-18 13:46:17 -0700

    Update fuckyou.py

--- fuckyou.py
+++ fuckyou.py
@@ -5,17 +5,19 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-  await client.change_presence(game=discord.Game(name="porno make me horno"))
+  await client.change_presence(activity = discord.Game(name="porno make me horno"))
 
 @client.event
 async def on_message(message):
 
     if message.author != client.user:
+      channel = message.channel
+
       if ("training" in message.content.lower()) or ("frau" in message.content.lower()) or ("sex" in message.content.lower()):
-        await client.send_message(message.channel,"OHHHHHHHHHHHHHHH YEAHHHHHHHHH!!!!!!!!!!!!!!!!!!!!!!")
+        await channel.send("OHHHHHHHHHHHHHHH YEAHHHHHHHHH!!!!!!!!!!!!!!!!!!!!!!")
        
       if ("poop" in message.content.lower()):
-        await client.send_message(message.channel,"I'M SHIDDING. SHIDDING AND FARDING.")
+        await channel.send("I'M SHIDDING. SHIDDING AND FARDING.")
 
 token = os.environ.get("DISCORD_BOT_SECRET")
 client.run(token)
