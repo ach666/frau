fcc32f547bef837e56fcb34de3568516e8705495
ach666 <46721480+ach666@users.noreply.github.com>
2019-06-19 14:42:37 -0700

    Update fuckyou.py

--- fuckyou.py
+++ fuckyou.py
@@ -19,8 +19,43 @@ async def on_message(message):
       elif ("poop" in message.content.lower()):
         await channel.send("I'M SHIDDING. SHIDDING AND FARDING.")
       
-      elif ("milk" in message.content.lower()) or ("prostate" in message.content.lower()):
+      elif ("milk" in message.content.lower()):
         await channel.send("milk me hard")
+      
+      elif ("hito wa mata" in message.content.lower()):
+        await channel.send("""hito wa mata oroka na sentaku wo kitto kurikaeshite iku darou
+tsumibukaku fujiyuu sou ni, soredemo asu wo sutekirezu kao wo ageru
+umareochita kono sekai de
+
+toki no kanata mimi wo sumasu musuu no Pieces of History
+katari kakeru koe naki koe
+tooi hi no kaze no nioi musekaeru netsu no kioku wa
+ikari wo hodoki arasoi nado hajime kara nakatta you ni
+
+nagashita chi mo namida mo wasure sararete iku
+daichi wa nando demo kagayaki wo torimodoshi
+
+hito wa mata oroka na sentaku wo kitto kurikaeshite iku darou
+tsumibukaku fujiyuu sou ni, soredemo asu wo sutekirezu kao wo ageru
+umareochita kono sekai de
+
+sasae aeta hi mo aru darou negawakuba Pieces of History
+warai kakeru kao naki kao
+taiyou to yume no nagori tamentai no kokoro no mama ni
+ayumi tsudzuketa odayakasa mo hageshisa mo kakae nagara
+
+subete wo tsutsumi konde toki wa nagarete iku
+matataku hoshi no ne ni ichinichi wo owarasete
+
+hito wa mata kanashii honnou de kitto machigaete shimau kedo
+sono tabi ni yari naoseru to shinjiru chikara mune no oku utagawanai
+umareochita sono toki kara
+
+hito wa mata oroka na sentaku wo kitto kurikaeshite iku darou
+tsumibukaku fujiyuu sou ni, soredemo asu wo sutekirezu ni
+hito wa mata kanashii honnou de kitto machigaete shimau kedo
+sono tabi ni yari naoseru to shinjiru chikara mune no oku utagawanai
+umareochita sono toki kara""")
 
 token = os.environ.get("DISCORD_BOT_SECRET")
 client.run(token)
